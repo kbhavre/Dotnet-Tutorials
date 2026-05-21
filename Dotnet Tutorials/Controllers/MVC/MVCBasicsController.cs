9d526cff5ae2c68b25a2c5dcdbbadca1964f36b2
kbhavre <kbhavre78@gmail.com>
2026-05-22 00:26:07 +0530

    Added new dropdown for MVC Tutorials

---
+++ Dotnet Tutorials/Controllers/MVC/MVCBasicsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Tutorials.Controllers.MVC
+{
+    public class MVCBasicsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ControllersAndActions()
+        {
+            return View();
+        }
+
+
+    }
+}
